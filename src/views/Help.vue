a8ce1926dba1f6ae761e756c728c2a0e323a7c82
maccyber <jonas.enge@gmail.com>
2019-10-30 15:31:38 +0100

    Fix help page

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -11,14 +11,18 @@
               <v-card-text>
                 <p>Nedenfor finner du ofte stilte spørsmål, svar og hurtigtaster.</p>
                 <p>Feil, mangler og ønsker kan du sende til <a href="mailto:post.opplaring@t-fk.no" title="Send e-post til post@fylkeskommune.no" target="_blank">post@fylkeskommune.no</a></p>
-                <div class="title text-uppercase">Om Louie</div>
+                <div class="title text-uppercase">Om Elevmappa</div>
                 <br />
-                <p>Louie gir deg mulighet til å søke opp elevene du er kontakt- og faglærer for.</p>
-                <p>På forsiden velger du en elev ved å trykke på navnet i listen. Når du har trykket på navnet til eleven får du sende varsel til eleven.</p>
+                <p>Elevmappa gir deg mulighet til å søke opp elevene du er kontakt- og faglærer for.<br/>I elevmappene vil du finne dokumenter som tilhører eleven.</p>
+                <p>På forsiden velger du en elev ved å trykke på navnet i listen.<br/>Når du har trykket på navnet til eleven får du se dokumentene du har leserettigheter til som lærer.</p>
                 <div class="title text-uppercase">Spørsmål</div>
                 <br />
                 <p><b>Jeg finner ikke eleven jeg leter etter.</b></p>
                 <p>Finner du ikke eleven du leter etter, ta kontakt med den som er ansvarlig for Extens eller administrativt personale på din skole.</p>
+                <p><b>Jeg mangler leserettigheter til et dokument jeg mener jeg bør lese.</b></p>
+                <p>Ta kontakt med din nærmeste leder som vil undersøke om du har rett til innsyn i dokumentet.</p>
+                <p><b>Jeg får melding om "Finner ikke elevmappe. Kontakt arkivet"</b></p>
+                <p>Ta kontakt med arkivet på e-post: post.arkiv@t-fk.no eller tlf. 35 91 70 11</p>
                 <div class="title text-uppercase">Hurtigtaster</div>
                 <br />
                 <p><strong>Dokumentvisning</strong></p>
